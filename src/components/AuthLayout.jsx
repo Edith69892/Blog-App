c8bcc94bad491f898c9d7871fc690bbb132f70e6
Roshan Patil <patilroshan9687@gmail.com>
2026-01-02 01:51:43 +0530

    add AuthLayout component for authentication handling and navigation

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom'
+
+
+function AuthLayout({ children, authentication = true }) {
+    const navigate = useNavigate();
+    const authStatus = useSelector(state => state.auth.status)
+
+    if (authentication && authStatus !== authentication) {
+        navigate("/")
+    } else if (!authentication && authStatus !== authentication) {
+        navigate('/login')
+    }
+
+    return (
+        <div>auth</div>
+    )
+}
+
+export default AuthLayout
