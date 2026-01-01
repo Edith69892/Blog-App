72cbf9d74962c509efa510c15f759d0371cdfca4
Roshan Patil <patilroshan9687@gmail.com>
2026-01-01 17:30:31 +0530

    import all components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
-import Header from "./Header";
+import Header from "./Header/Header";
 import Footer from "./Footer";
+import Container from "./container/Container"
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn"
 
-export {Header, Footer}
+
+export {Header, Footer, Logo, LogoutBtn}
